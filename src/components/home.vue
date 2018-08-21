4a3ebd09e9a5d739ca6ee97499415f7801d04773
David Salinas <saotand@gmail.com>
2018-08-21 11:08:24 -0400

    update dashboard

--- src/components/home.vue
+++ src/components/home.vue
@@ -16,6 +16,18 @@
   </v-container>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
---
+++ src/components/user/custom/profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-card>
+        <v-card-media>
+          <v-card-title>
+            Nombre de Usuario
+          </v-card-title>
+        </v-card-media>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
